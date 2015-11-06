314d57231bf4efecd1313c44940be4d701e1fd9d
hayyuhanifah <heripottypot@gmail.com>
2015-11-06 16:28:31 +0700

    temporary commit

---
+++ src/ruleBasedClassifier/RuleBasedClassifier.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ruleBasedClassifier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Hayyu' Luthfi Hanifah
+ */
+public class RuleBasedClassifier {
+    private List<String> dictionary1 = new ArrayList<String>();
+    private List<String> dictionary2 = new ArrayList<String>();
+    private List<String> dictionary3 = new ArrayList<String>();
+    
+    
+}
